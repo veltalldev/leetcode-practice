dfca109cb4d51bc692d2acda267f6c1e31609759
Velt Krapfenwald <veltalldev@gmail.com>
2024-09-06 18:44:21 -0700

    modify 0906 // add commentary to clarify the solution

--- 20240906/lib/naive.dart
+++ 20240906/lib/naive.dart
@@ -11,18 +11,23 @@ int findLargestContiguousSum(List<int> nums) {
   int maxSum = 0;
   int runningSumLeft = 0;
   int runningSumRight = 0;
+
   // start with the "widest" subarray
   int i = 0;
   int j = nums.length - 1;
   if (nums.length == 0) {
     return maxSum;
   }
+
+  // going through the entire list both ways
   while (i <= nums.length - 1 && j >= 0) {
+    // roll the smaller side
     if (nums[i] <= nums[j]) {
       runningSumLeft += nums[i++]; // record the value then move on
     } else if (nums[i] >= nums[j]) {
       runningSumRight += nums[j--];
     }
+    // trim off the subsequence with negative sum
     if (runningSumLeft < 0) {
       runningSumLeft = 0;
       continue;
@@ -31,6 +36,7 @@ int findLargestContiguousSum(List<int> nums) {
       runningSumRight = 0;
       continue;
     }
+    // update max
     maxSum = max(
       maxSum,
       max(
