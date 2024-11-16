71a242181183ac6f3764a802e7763541094a5153
Velt Krapfenwald <veltalldev@gmail.com>
2024-11-15 22:49:38 -0800

    test: Redesign test suite
    - Cover Error Handling cases
    - Organize tests into appropriate groups
    - Use more descriptive messages
    - Comprehensively test each individual factor
        - separate tests for each sequence length
        - separate tests for each byte

--- 20241114/test/utf8_validator_test.dart
+++ 20241114/test/utf8_validator_test.dart
@@ -3,62 +3,55 @@ import 'package:leetcode20241114/utf8_validator.dart';
 
 void main() {
   group('UTF8Validator', () {
-    late UTF8Validator utf8Validator;
+    late UTF8Validator validator;
 
     setUp(() {
-      utf8Validator = UTF8Validator();
+      validator = UTF8Validator();
     });
 
-    // UTF8_Validator does not have any data to initialize
-    // skipping test
+    group('Input validation', () {
+      test('throws ArgumentError for unsupported types', () {});
 
-    group('validate', () {
-      test('string validation not supported, should return false', () {
-        final utf8BitString = '11100010 10000010 10101100';
-        final isValid = utf8Validator.validate(utf8BitString);
-        expect(isValid, isFalse);
+      test('throws ArgumentError for string input', () {});
+
+      test('throws RangeError for byte values out of valid range', () {});
+    });
+
+    group('Empty Sequence validation', () {
+      test('validates empty byte sequence as true', () {});
+    });
+
+    group('single byte validation', () {
+      test('validates ASCII characters (0-127)', () {});
+
+      test('invalidates bytes with MSB set', () {});
+    });
+
+    group('two byte sequence validation', () {
+      test('validates valid two byte sequences', () {
+        // Example: Latin small letter a with acute
+      });
+      test('invalidates sequences with wrong continuation byte', () {});
+    });
+
+    group('three byte sequence validation', () {
+      test('validates valid three byte sequences', () {
+        // Example: Euro sign
+      });
+      test('invalidates sequences with wrong continuation bytes', () {});
+    });
+
+    group('four byte sequence validation', () {
+      test('validates valid four byte sequences', () {
+        // Example: Summation symbol / Greek capital Sigma
       });
 
-      group('byte sequence validation', () {
-        test('valid empty input', () {
-          final bytes = <int>[];
-          final isValid = utf8Validator.validate(bytes);
-          expect(isValid, isTrue);
-        });
-        test('valid single byte', () {
-          final bytes = [65];
-          final isValid = utf8Validator.validate(bytes);
-          expect(isValid, isTrue);
-        });
-        test('valid multi-byte', () {
-          final bytes = [230, 130, 172]; // '€'
-          final isValid = utf8Validator.validate(bytes);
-          expect(isValid, isTrue);
-        });
+      test('invalidates sequences with wrong continuation bytes', () {});
+    });
 
-        test('invalid byte sequence: invalid byte value range', () {
-          final bytes = [230, 256, 172];
-          final isValid = utf8Validator.validate(bytes);
-          expect(isValid, isFalse);
-        });
-
-        test('invalid byte sequence: more than 4 bytes', () {
-          final bytes = [230, 130, 172, 143, 143];
-          final isValid = utf8Validator.validate(bytes);
-          expect(isValid, isFalse);
-        });
-
-        test('invalid byte sequence: bad leading byte', () {
-          final bytes = [170, 130, 172];
-          final isValid = utf8Validator.validate(bytes);
-          expect(isValid, isFalse);
-        });
-
-        test('invalid byte sequence: bad continuation byte(s)', () {
-          final bytes = [230, 202, 138];
-          final isValid = utf8Validator.validate(bytes);
-          expect(isValid, isFalse);
-        });
+    group('sequence length validation', () {
+      test('invalidates sequences longer than 4 bytes', () {
+        expect(validator.validate([1, 1, 1, 1, 1]), isFalse);
       });
     });
   });
